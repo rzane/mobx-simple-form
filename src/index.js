0f2c789feded58c8e1a4c8e1ad0f76190de293d5
Ray Zane <ray@promptworks.com>
2016-12-04 23:49:02 -0500

    Add a build step to conver to es5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import * as _casters from './casters';
+import * as _validations from './validations';
+
 export { default as Form } from './Form';
 export { default as Field } from './Field';
 export { default as FieldObject } from './FieldObject';
 export { default as FieldArray } from './FieldArray';
+
+export const casters = _casters;
+export const validations = _validations;
