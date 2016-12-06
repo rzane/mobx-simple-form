cea3b9e39012a88cfaf9f3f34ed8478a44e6b15e
Ray Zane <ray@promptworks.com>
2016-12-05 19:54:04 -0500

    Change exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import * as _casters from './casters';
-import * as _validations from './validations';
+import Form from './Form';
 
-export { default as Form } from './Form';
+export * from './casters';
+export * from './validations';
+
 export { default as Field } from './Field';
 export { default as FieldObject } from './FieldObject';
 export { default as FieldArray } from './FieldArray';
 
-export const casters = _casters;
-export const validations = _validations;
+export default Form;
